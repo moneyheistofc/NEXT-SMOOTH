2f0d169aaf2c120973319ea8035dcb41531cd58e
KARIᴴᵀᵀᴾ😌 <moneyheistofc@gmail.com>
2025-11-06 20:06:52 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -14,9 +14,6 @@ export default function Home() {
         <Projects />
         <Footer />
       </div>
-      
-      {/* Font Awesome CDN */}
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
     </main>
   );
     }
