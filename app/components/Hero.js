9b00f9a3cbaa06b5af78967f7ee73966178662a6
KARIᴴᵀᵀᴾ😌 <moneyheistofc@gmail.com>
2025-11-06 21:54:06 +0530

    Update Hero.js

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -4,13 +4,13 @@ export default function Hero() {
       <div className="profile-container">
         <div className="light-ring"></div>
         <img 
-          src="https://raw.githubusercontent.com/maverick455456/ADMINS/refs/heads/main/ADMIN-DP/MR NIPUN OFC.png" 
+          src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80" 
           alt="MR NIPUN" 
           className="profile-image"
         />
       </div>
-      <h1>MR NIPUN OFC / N-TECH</h1>
+      <h1>MR NIPUN OFC / TECH-WEB</h1>
       <p>Full Stack Developer | UI/UX Designer | Tech Enthusiast</p>
     </section>
   );
-            }
+}
